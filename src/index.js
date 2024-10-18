039675fe1a01ba576899d3f768f9afb5893580fd
annedube30 <annedube30@gmail.com>
2024-10-18 15:29:02 +0200

    added id form and on javascript added typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter(".poem", {
+    strings: ["english poem"],
+    autoStart: true,
+    delay: 3,
+    cursor: "",
+  });
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
